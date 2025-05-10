615f354071a6ff57a9ad4cf2546fa6248cf6e875
Manish <manish@gmail.com>
2025-05-10 09:43:36 +0530

    Updated the message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from 3RI technology');
+  res.send('Hello World from 3RI technology.');
 });
 
 app.listen(port, () => {
